8e19867083a796efbca4eae65bf7c01932859e5d
sahil <sbhandari791@gmail.com>
2020-05-17 00:07:56 +0530

    RearrangeArray.java

---
+++ Math/RearrangeArray.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public void arrange(ArrayList<Integer> a) {
+        int n = a.size();
+        for(int i=0;i<a.size();i++){
+            a.set(i,a.get(i) + (a.get(a.get(i))%n) * n);
+        }
+        
+        for(int i=0;i<a.size();i++){
+            a.set(i,a.get(i)/n);
+        }
+    }
+}
